60376113cb6c13884c0a85662e1b92ba314fdb13
Nicholas Lamicela <nupanick@gmail.com>
2024-05-27 19:32:24 -0700

    port discord mod from old bot

--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
 import { Bot } from "./lib";
 import { ModConsole } from "./mod-console";
+import { ModDiscord } from "./mod-discord";
 import { ModPing } from "./mod-ping";
 import { ModTictactoe } from "./mod-tictactoe";
 
 function main() {
     const bot = new Bot();
     bot.addMod(new ModConsole());
+    bot.addMod(new ModDiscord());
     bot.addMod(new ModPing());
     bot.addMod(new ModTictactoe());
     bot.start();
---
+++ mod-discord.ts
@@ -0,0 +1,77 @@
+import { Client, Events, GatewayIntentBits, TextChannel, userMention } from 'discord.js';
+import { readFileSync, writeFileSync } from 'fs';
+import { Action, ModBase } from './lib';
+
+type Config = {
+    prefix: string;
+    token: string;
+    channel: string;
+};
+
+const DEFAULT_CONFIG = {
+    prefix: '!',
+    token: '',
+    channel: '',
+};
+
+function readWriteConfig(path = 'config.json') {
+    let config: Config;
+    try {
+        const data = readFileSync(path, { encoding: 'utf-8' });
+        config = { ...DEFAULT_CONFIG, ...JSON.parse(data) };
+    } catch {
+        console.log("Can't read config! Using defaults.");
+        config = DEFAULT_CONFIG;
+    }
+    writeFileSync(path, JSON.stringify(config, null, 2));
+    return config;
+}
+
+export class ModDiscord extends ModBase {
+    config: Config;
+    client: Client;
+    channel?: TextChannel;
+
+    constructor() {
+        super();
+        this.config = readWriteConfig();
+        this.client = new Client({ intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.MessageContent,
+        ]});
+        this.start();
+    }
+
+    async start() {
+        this.client.once(Events.ClientReady, async (c) => {
+            console.log(`[discord] connected as ${c.user.tag}`);
+            const ch = await c.channels.fetch(this.config.channel);
+            if (!ch?.isTextBased) {
+                console.log("[discord] can't connect to non-text channel");
+                return;
+            }
+            this.channel = ch as TextChannel;
+            this.write('[discord] app started.');
+        });
+
+        this.client.on(Events.MessageCreate, (m) => {
+            if (!m.content.startsWith(this.config.prefix)) return;
+            const text = m.content.slice(this.config.prefix.length);
+            const user = userMention(m.author.id);
+            this.write_in(text, user);
+        });
+
+        await this.client.login(this.config.token);
+    }
+
+    async handle(action: Action) {
+        if (action.type === 'text_out') {
+            if (!this.channel) {
+                console.log('[discord] error: unable to write to channel');
+                return;
+            }
+            await this.channel.send(action.text);
+        }
+    }
+}
