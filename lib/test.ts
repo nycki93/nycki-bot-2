0b9e1959aec484094e42e21e8a0499f044c17f0b
Nick Lamicela <nupanick@gmail.com>
2024-06-02 19:33:36 -0700

    lib refactor

---
+++ lib/test.ts
@@ -0,0 +1,44 @@
+import { AsyncQueue } from "./async-queue";
+import { Bot } from "./bot";
+import { Event } from "./event";
+
+export class TestBot extends Bot {
+    log = new AsyncQueue<Event>();
+    timeoutMs = 5000;
+
+    send(event: Event) {
+        super.send(event);
+        this.log.push(event);
+    }
+
+    inject(event: Event) {
+        this.send(event);
+        return this.expect(event);
+    }
+
+    async next(timeoutMs=this.timeoutMs) {
+        let timeout: NodeJS.Timeout;
+        const t = new Promise<Event>((_, rej) => timeout = setTimeout(
+            () => rej(new Error(`[testbot] did not receive an event within ${timeoutMs} ms.`)),
+            timeoutMs,
+        ));
+        const p = new Promise<Event>(async (res) => {
+            const event = await this.log.shift();
+            clearTimeout(timeout);
+            res(event);
+        });
+        return Promise.race([p, t]);
+    }
+
+    async expect(expected: Event, timeoutMs=this.timeoutMs) {
+        const actual = await this.next(timeoutMs);
+        const keys = Object.keys(expected) as (keyof Event)[]
+        if (
+            keys.length == Object.keys(actual).length
+            && keys.every((k) => expected[k] === actual[k])
+        ) {
+            return;
+        }
+        throw new Error(`Expected:\n${JSON.stringify(expected)}\nreceived:\n${JSON.stringify(actual)}`);
+    }
+}
