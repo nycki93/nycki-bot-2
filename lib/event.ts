03144ffee003e3c895012c4f059827c1dc293959
Nick Lamicela <nupanick@gmail.com>
2024-06-02 19:42:05 -0700

    event equality check

--- lib/event.ts
+++ lib/event.ts
@@ -25,6 +25,15 @@ export namespace Event {
     }
 
     export type Event = Input | Write;
+
+    export function equals(a: Event, b: Event) {
+        const keys = Object.keys(a) as (keyof Event)[];
+        if (keys.length !== Object.keys(b).length) return false;
+        for (const k of keys) {
+            if (a[k] !== b[k]) return false;
+        }
+        return true;
+    }
 }
 
 export type Event = Event.Event;
